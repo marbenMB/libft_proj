f9776fcf409b65ae6b0fa9c3eeb437094b58b22e
Marouane  Benbajja <mbenbajj@e1r3p1.1337.ma>
2021-11-26 22:27:51 +0100

    opptimize the while condition in ft_lstlast

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -6,20 +6,17 @@
 /*   By: mbenbajj <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 19:34:50 by mbenbajj          #+#    #+#             */
-/*   Updated: 2021/11/20 19:52:35 by mbenbajj         ###   ########.fr       */
+/*   Updated: 2021/11/26 22:21:45 by mbenbajj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 t_list	*ft_lstlast(t_list *lst)
-{
-	t_list	*now;
-
+{	
 	if (!lst)
 		return (NULL);
-	now = lst;
-	while (now && now -> next)
-		now = now -> next;
-	return (now);
+	while (lst -> next)
+		lst = lst -> next;
+	return (lst);
 }
